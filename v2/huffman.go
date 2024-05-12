b3f16c8a6f5974e23d123b1880623dd463a5dd09
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-05-12 22:14:22 +0200

    v2: byte frequency counting for tree building

--- v2/huffman.go
+++ v2/huffman.go
@@ -1,1 +1,59 @@
+// tcmpr2 is a huffman conding lossless compression algorithm (see [1]). tcmpr
+// compressed blocks of data use the 0x74, 0x63, 0x6d, 0x70, 0x72, 0x32, 0x0A
+// magic number (tcmpr2).
+//
+// [1]: https://en.wikipedia.org/wiki/Huffman_coding
 package v2
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+)
+
+var magicNum = [...]byte{0x74, 0x63, 0x6d, 0x70, 0x72, 0x32, 0x0A}
+
+type huffman struct {
+	// TODO: think about serializing the huffmann coding tree and prefixing the serialized bytes with it.
+	key byte
+	l   *huffman
+	r   *huffman
+}
+
+func createTree(r *bufio.Reader) (*huffman, error) {
+	b := &bytes.Buffer{}
+	tee := bufio.NewReader(io.TeeReader(r, b))
+	freq := map[byte]int{}
+	for {
+		c, err := tee.ReadByte()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			} else {
+				return nil, err
+			}
+		}
+		if val, ok := freq[c]; ok {
+			freq[c] = val + 1
+		} else {
+			freq[c] = 1
+		}
+	}
+	fmt.Println(freq)
+	return &huffman{}, nil
+}
+
+func Compress(r io.Reader, w io.Writer) error {
+	h, err := createTree(bufio.NewReader(r))
+	if err != nil {
+		return err
+	}
+	fmt.Println(h)
+	panic("Not implemented")
+}
+
+func Decompress(r io.Reader, w io.Writer) error {
+	panic("Not implemented")
+}
--- v2/huffman_test.go
+++ v2/huffman_test.go
@@ -1,1 +1,35 @@
 package v2
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHuffman(t *testing.T) {
+	input := []struct {
+		in  string
+		exp string
+	}{
+		{"BCAADDDCCACACAC", ""},
+	}
+
+	for _, test := range input {
+		inBuf := strings.NewReader(test.in)
+		outBuf := bytes.Buffer{}
+		err := Compress(inBuf, &outBuf)
+		assert.NoError(t, err, "Failed to compress buffer")
+
+		b := outBuf.Bytes()
+		fmt.Printf("exp: %#+v\nout: %#+v\n len in: %d, len out: %d\n", test.exp, b, len([]byte(test.in)), len(b))
+		assert.Equal(t, test.exp, outBuf.Bytes())
+
+		outBuf2 := bytes.Buffer{}
+		err = Decompress(&outBuf, &outBuf2)
+		assert.NoError(t, err, "Failed to decompress buffer")
+		assert.Equal(t, []byte(test.in), outBuf2.Bytes())
+	}
+}
